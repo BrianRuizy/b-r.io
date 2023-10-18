cffac63ebc8b52eedeb22443ea2d8326e28c1d56
Brian Ruiz <brianruiz123@yahoo.com>
2023-10-17 20:32:17 -0500

    prevent views from incrementing in local

--- app/api/hitsSlug/route.ts
+++ app/api/hitsSlug/route.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { type NextRequest } from 'next/server'
+import { NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
 
@@ -12,40 +12,53 @@ export async function GET(req: NextRequest) {
       return new Response("Please provide a slug", { status: 200 });
     }
 
-    const post = await prisma.post.findUnique({
-      where: {
-        slug: slug as string,
-      },
-    });
+    let viewCount = 0;
 
-    if (!post) {
-      const newPost = await prisma.post.create({
-        data: {
+    if (process.env.NODE_ENV !== "development") {
+      const post = await prisma.post.findUnique({
+        where: {
           slug: slug as string,
-          views: 1,
         },
       });
-      return new Response(`Created new post with slug '${newPost.slug}'`, {
-        status: 201,
-      }); 
+
+      if (!post) {
+        const newPost = await prisma.post.create({
+          data: {
+            slug: slug as string,
+            views: 1,
+          },
+        });
+        viewCount = newPost.views;
+      } else {
+        const updatedPost = await prisma.post.update({
+          where: {
+            id: post.id,
+          },
+          data: {
+            views: post.views + 1,
+          },
+        });
+        viewCount = updatedPost.views;
+      }
+    } else {
+      // Retrieve the view count without incrementing in the local environment
+      const post = await prisma.post.findUnique({
+        where: {
+          slug: slug as string,
+        },
+      });
+
+      if (post) {
+        viewCount = post.views;
+      }
     }
 
-    if (post) {
-      const updatedPost = await prisma.post.update({
-        where: {
-          id: post.id,
-        },
-        data: {
-          views: post.views + 1,
-        },
-      });
-      return new Response(JSON.stringify({ Views: updatedPost.views }), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-          }
+    return new Response(JSON.stringify({ Views: viewCount }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   } catch (error) {
     console.error(error);
     return new Response("Internal Server Error", { status: 500 });
