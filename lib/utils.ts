a2e3d4629bba5b02a011e27213d8ecf03da8be2c
Decloquement Oscar <odecloquement@gmail.com>
2023-10-24 01:46:45 +0200

    Update regex

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,5 @@
 
 export function addCommas(x: any) {
   if (x === undefined) return;
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
