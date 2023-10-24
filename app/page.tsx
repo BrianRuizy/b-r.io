55ba0a74d9cd5b097ddd74f200cdd52830c0ef20
Brian Ruiz <brianruiz123@yahoo.com>
2023-10-24 01:17:15 -0500

    revert home page change

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,27 @@ export default async function Home() {
   const posts = allPosts
     .sort(
       (a, b) =>
-        new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+        new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime(),
     )
     // 3 most recent
     .filter((_, i) => i < 3);
 
   return (
     <div className="flex flex-col gap-16 md:gap-24">
-      <div className="flex flex-col gap-8 animate-in">
+      <div className="flex animate-in flex-col gap-8">
         <div>
-          <h1 className="animate-in text-3xl font-bold tracking-tight text-primary bg-gradient-to-br from-blue-600 to-green-500 inline-block text-transparent bg-clip-text">
+          <h1 className="animate-in text-3xl font-bold tracking-tight text-primary">
             Brian Ruiz
           </h1>
           <p
-            className="text-secondary animate-in"
+            className="animate-in text-secondary"
             style={{ "--index": 1 } as React.CSSProperties}
           >
             I write code and make videos.
           </p>
         </div>
         <div
-          className="animate-in flex flex-col md:flex-row gap-6 text-secondary md:items-center"
+          className="flex animate-in flex-col gap-6 text-secondary md:flex-row md:items-center"
           style={{ "--index": 1 } as React.CSSProperties}
         >
           <Image
@@ -44,7 +44,7 @@ export default async function Home() {
           <Stats />
         </div>
         <p
-          className="text-primary max-w-lg animate-in"
+          className="max-w-lg animate-in text-primary"
           style={{ "--index": 2 } as React.CSSProperties}
         >
           Hi, I&apos;m Brian Ruiz, a software engineer who loves building cool
@@ -52,36 +52,39 @@ export default async function Home() {
           where I focus on tech, creative vlogs, and personal development.
         </p>
         <ul
-          className="flex flex-col md:flex-row gap-2 md:gap-6 text-secondary animated-list animate-in"
+          className="animated-list flex animate-in flex-col gap-2 text-secondary md:flex-row md:gap-6"
           style={{ "--index": 2 } as React.CSSProperties}
         >
           <li className="transition-opacity">
             <Link
               href="mailto:contact@b-r.io"
-              className="flex gap-2 items-center no-underline"
+              className="flex items-center gap-2 no-underline"
             >
-              <ArrowUpRightIcon className="w-5 h-5" />
+              <ArrowUpRightIcon className="h-5 w-5" />
               <span>Email me</span>
             </Link>
           </li>
           <li className="transition-opacity">
             <Link
               href="/links"
-              className="flex gap-2 items-center no-underline"
+              className="flex items-center gap-2 no-underline"
             >
-              <ArrowUpRightIcon className="w-5 h-5" />
+              <ArrowUpRightIcon className="h-5 w-5" />
               <span>More ways to connect</span>
             </Link>
           </li>
         </ul>
       </div>
       <div
-        className="flex flex-col gap-8 animate-in"
+        className="flex animate-in flex-col gap-8"
         style={{ "--index": 3 } as React.CSSProperties}
       >
         <h2 className="text-secondary">Latest Posts</h2>
         <PostList posts={posts} />
-        <Link href="/blog" className="text-secondary hover:text-primary underline underline-offset-4">
+        <Link
+          href="/blog"
+          className="text-secondary underline underline-offset-4 hover:text-primary"
+        >
           See All
         </Link>
       </div>
