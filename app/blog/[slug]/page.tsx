0b2882904455b5b12347e9348262eddc3504986f
Decloquement Oscar <odecloquement@gmail.com>
2023-11-01 01:37:15 +0100

    Update content, structure

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,21 +1,71 @@
 import { allPosts, Post as PostType } from ".contentlayer/generated";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import type { Metadata, ResolvingMetadata } from "next";
 
 import Mdx from "@/app/blog/components/ui/MdxWrapper";
 import ViewCounter from "@/app/blog/components/ui/ViewCounter";
 import PostList from "@/app/blog/components/ui/PostList";
-import Parallax from "@/app/blog/components/mdx/parallax";
 import Tags from "@/components/Tags";
 import Link from "@/components/ui/Link";
 import { formatDate } from "lib/formatdate";
+
+import Avatar from "@/public/profile_picture.png";
 
 type PostProps = {
   post: PostType;
   related: PostType[];
 };
 
-export default function Post({ params }: { params: any }) {
+type Props = {
+  params: {
+    slug: string;
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  const {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+    slug,
+  } = post;
+
+  const metadata: Metadata = {
+    title: `${title} | Oscar Decloquement`,
+    description,
+    openGraph: {
+      title: `${title} | Oscar Decloquement`,
+      description,
+      type: "article",
+      publishedTime,
+      url: `https://o-d.me/blog/${title}`,
+      images: [
+        {
+          url: `https://o-d.me/api/og?title=${title}`,
+          alt: title,
+        },
+      ],
+
+    },
+  };
+
+  return metadata;
+}
+
+export default async function Post({ params }: { params: any }) {
   const post = allPosts.find((post) => post.slug === params.slug);
 
   if (!post) {
@@ -25,24 +75,41 @@ export default function Post({ params }: { params: any }) {
   return (
     <div className="flex flex-col gap-20">
       <article>
-        <div className="flex flex-col gap-3 animate-in max-w-xl">
-          <p className="text-secondary">
-            <time dateTime={post.publishedAt}>
-              {formatDate(post.publishedAt)}
-            </time>
-            {post.updatedAt ? `(Updated ${formatDate(post.updatedAt)})` : ""}
-            {" • "}
-            <ViewCounter post={post} />
-          </p>
-          <h1 className="text-primary text-3xl font-bold tracking-tight leading-tight">
-            {post.title}
-          </h1>
-          <p
-            className="text-lg md:text-xl text-secondary animate-in leading-tight"
-            style={{ "--index": 1 } as React.CSSProperties}
-          >
-            {post.summary}
-          </p>
+        <div
+          className="flex animate-in flex-col gap-8"
+          style={{ "--index": 1 } as React.CSSProperties}
+        >
+          <div className="max-w-xl space-y-2">
+            <h1 className="text-3xl font-bold leading-tight tracking-tight text-primary">
+              {post.title}
+            </h1>
+            <p className="text-lg leading-tight text-secondary md:text-xl">
+              {post.summary}
+            </p>
+          </div>
+
+          <div className="flex max-w-none items-center gap-4">
+            <Image
+              src={Avatar}
+              width={40}
+              height={40}
+              alt="avatar"
+              className="rounded-full bg-secondary"
+            />
+            <div className="leading-tight">
+              <p className="font-medium text-primary">Brian Ruiz</p>
+              <p className="text-secondary">
+                <time dateTime={post.publishedAt}>
+                  {formatDate(post.publishedAt)}
+                </time>
+                {post.updatedAt
+                  ? `(Updated ${formatDate(post.updatedAt)})`
+                  : ""}
+                {" · "}
+                <ViewCounter post={post} />
+              </p>
+            </div>
+          </div>
         </div>
         {post.image && (
           <>
@@ -52,7 +119,7 @@ export default function Post({ params }: { params: any }) {
               alt={`${post.title} post image`}
               width={700}
               height={350}
-              className="w-[calc(100%+48px)] -ml-6 lg:w-[calc(100%+128px)] lg:-ml-16 md:rounded-lg max-w-none animate-in"
+              className="-ml-6 w-[calc(100%+48px)] max-w-none animate-in md:rounded-lg lg:-ml-16 lg:w-[calc(100%+128px)]"
               style={{ "--index": 2 } as React.CSSProperties}
               priority
               quality={100}
@@ -62,7 +129,7 @@ export default function Post({ params }: { params: any }) {
 
         <div className="h-16" />
         <div
-          className="prose animate-in prose-neutral"
+          className="prose prose-neutral animate-in"
           style={{ "--index": 3 } as React.CSSProperties}
         >
           <Mdx code={post.body.code} />
