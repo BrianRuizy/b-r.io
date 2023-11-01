1e76daa52c1eeef8a26a64547759224d9014b93e
Decloquement Oscar <odecloquement@gmail.com>
2023-11-01 01:35:59 +0100

    Update content

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "@/components/ui/Link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "404 | Brian Ruiz",
+  title: "404 | Oscar Decloquement",
   description:
     "Uh oh! This page does not exist",
 };
@@ -11,8 +11,7 @@ const Custom404 = (): JSX.Element => (
   <div className="flex flex-col gap-2">
     <h1>404 - Page not found</h1>
     <p className="text-secondary">
-      Uh oh! This page does not exists, maybe you clicked an old link or
-      misspelled. Please try again…
+      Uh oh! Cette page n&apos;existe pas, vous avez surement cliqué sur un mauvais lien. Merci de réessayer…
     </p>
     <div className="h-2" />
     <Link href="/" underline>
