e0745375777d80e8275abb8b468268de01fa6d4c
Decloquement Oscar <odecloquement@gmail.com>
2023-11-03 19:55:55 +0100

    Update error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,13 +9,13 @@ export const metadata: Metadata = {
 
 const Custom404 = (): JSX.Element => (
   <div className="flex flex-col gap-2">
-    <h1>404 - Page not found</h1>
+    <h1>404 - Page non trouvée</h1>
     <p className="text-secondary">
-      Uh oh! Cette page n&apos;existe pas, vous avez surement cliqué sur un mauvais lien. Merci de réessayer…
+      Oh! Cette page n&apos;existe pas, vous avez surement cliqué sur un mauvais lien. Merci de réessayer…
     </p>
     <div className="h-2" />
     <Link href="/" underline>
-      Return home
+      Retourner sur la page d&apos;accueil
     </Link>
   </div>
 );
