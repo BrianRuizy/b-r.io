76c0008a26e6920ce94407283b7324928581c52e
Brian Ruiz <brianruiz123@yahoo.com>
2023-11-07 18:56:49 -0600

    fix form dark mode

--- app/blog/components/ui/NewsletterSignupForm.tsx
+++ app/blog/components/ui/NewsletterSignupForm.tsx
@@ -72,7 +72,7 @@ export default function NewsletterSignupForm() {
   }
 
   return (
-    <Halo>
+    <Halo strength={5}>
       <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-tertiary p-6 text-center lg:p-8">
         <p className="font-medium text-primary">Subscribe to my newsletter</p>
         <p className="max-w-md text-secondary">
@@ -91,11 +91,11 @@ export default function NewsletterSignupForm() {
               type="email"
               name={name}
               id="email"
-              className="block w-full rounded-md  px-4 py-1.5 text-neutral-900 border border-primary placeholder:text-neutral-400 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md  px-4 py-1.5 text-primary border border-primary placeholder:text-tertiary focus:ring-inset focus:ring-indigo-600 bg-primary"
               placeholder="you@example.com"
             />
           </div>
-          <button className="w-full whitespace-nowrap rounded-md bg-neutral-800 px-4 py-1.5 text-white hover:bg-neutral-900 focus:ring-inset focus:ring-indigo-600 focus-visible:outline focus-visible:outline-2 dark:bg-neutral-200 dark:text-black hover:dark:bg-neutral-100 md:w-fit ">
+          <button className="w-full whitespace-nowrap rounded-md bg-neutral-800 px-4 py-1.5 text-white hover:bg-neutral-900 focus:ring-inset focus:ring-indigo-600 focus-visible:outline focus-visible:outline-2 dark:bg-neutral-300 dark:text-black hover:dark:bg-neutral-100 md:w-fit ">
             {"Sign up"}
           </button>
         </form>
