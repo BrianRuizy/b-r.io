4c876bc6ea44dae1527b648b452f420188f7c527
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-05 00:50:22 -0500

    update 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,13 +3,12 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "404 | Brian Ruiz",
-  description:
-    "Uh oh! This page does not exist",
+  description: "Uh oh! This page does not exist",
 };
 
 const Custom404 = (): JSX.Element => (
   <div className="flex flex-col gap-2">
-    <h1>404 - Page not found</h1>
+    <h1 className="text-3xl font-bold tracking-tight text-primary">404</h1>
     <p className="text-secondary">
       Uh oh! This page does not exists, maybe you clicked an old link or
       misspelled. Please try again…
