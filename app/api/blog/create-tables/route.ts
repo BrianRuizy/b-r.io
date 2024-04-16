fb4bec95c8d43b51e81d145cbf8e36fc58047346
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-16 16:08:05 -0500

    Add @vercel/postgres package and blogviews routes handler

--- app/api/blog/create-tables/route.ts
+++ app/api/blog/create-tables/route.ts
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   try {
     const result = await sql`
       CREATE TABLE IF NOT EXISTS BlogViews (
