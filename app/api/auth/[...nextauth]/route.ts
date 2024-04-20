81c8cd17915abe6d1a2c758698fb8bee8107a508
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-20 18:53:41 -0500

    Revert "temp remove auth callback"
    This reverts commit 77bab11ccc444dea6dd1d43cbf07434ad43e3d63.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -22,31 +22,31 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    // async session({ session }) {
-    //   if (session) {
-    //     // Check if user already exists in authors table
-    //     const existingAuthor =
-    //       await sql`SELECT * FROM authors WHERE email = ${session.user.email}`.then(
-    //         (res) => res.rows[0],
-    //       );
+    async session({ session }) {
+      if (session) {
+        // Check if user already exists in authors table
+        const existingAuthor =
+          await sql`SELECT * FROM authors WHERE email = ${session.user.email}`.then(
+            (res) => res.rows[0],
+          );
 
-    //     let authorId;
-    //     if (existingAuthor) {
-    //       // If user already exists, use their id
-    //       authorId = existingAuthor.id;
-    //     } else {
-    //       // If user does not exist, add them to authors table and use the returned id
-    //       authorId =
-    //         await sql`INSERT INTO authors (name, email, image) VALUES (${session.user.name}, ${session.user.email}, ${session.user.image}) RETURNING id`.then(
-    //           (res) => res.rows[0].id,
-    //         );
-    //     }
+        let authorId;
+        if (existingAuthor) {
+          // If user already exists, use their id
+          authorId = existingAuthor.id;
+        } else {
+          // If user does not exist, add them to authors table and use the returned id
+          authorId =
+            await sql`INSERT INTO authors (name, email, image) VALUES (${session.user.name}, ${session.user.email}, ${session.user.image}) RETURNING id`.then(
+              (res) => res.rows[0].id,
+            );
+        }
 
-    //     // Add id to session.user
-    //     session.user.id = authorId;
-    //   }
-    //   return session;
-    // },
+        // Add id to session.user
+        session.user.id = authorId;
+      }
+      return session;
+    },
   },
 });
 
