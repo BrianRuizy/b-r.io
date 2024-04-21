c10374ec4c6aca5b8368356498b5fc08458069ef
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-20 22:14:37 -0500

    Refactor User component to remove commented out code and unused imports

--- app/community/components/User.tsx
+++ app/community/components/User.tsx
@@ -5,9 +5,6 @@ import * as Avatar from "@radix-ui/react-avatar";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { FaGithub } from "react-icons/fa";
 
-import { Drawer } from "vaul";
-import clsx from "clsx";
-
 export default function User() {
   const { data: session } = useSession();
 
@@ -62,70 +59,6 @@ export default function User() {
             </DropdownMenu.Content>
           </DropdownMenu.Portal>
         </DropdownMenu.Root>
-        {/* <Drawer.Root
-          snapPoints={[.4, .99]}
-        >
-          <Drawer.Trigger asChild>
-            <div
-              className={clsx(
-                "fixed bottom-6 right-6 transition-all md:hidden",
-                !session ? "hidden" : "visible",
-              )}
-            >
-              <Avatar.Root className="inline-flex h-14 w-14 select-none items-center justify-center overflow-hidden rounded-full border border-secondary bg-secondary align-middle">
-                <Avatar.Image
-                  className="h-full w-full rounded-[inherit] object-cover"
-                  src={session?.user?.image ?? ""}
-                  alt="Avatar image"
-                />
-                <Avatar.Fallback
-                  className="flex h-full w-full items-center justify-center border border-secondary bg-secondary text-sm font-medium text-primary"
-                  delayMs={600}
-                >
-                  {session?.user?.name?.slice(0, 2) ?? ""}
-                </Avatar.Fallback>
-              </Avatar.Root>
-            </div>
-          </Drawer.Trigger>
-          <Drawer.Portal>
-            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-
-            <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 h-[96%] rounded-t-[10px] bg-primary outline-none">
-              <div className="h-full flex-1 flex-col gap-6 rounded-t-[10px] bg-primary ">
-                <div className="fixed left-0 right-0 top-1.5 mx-auto flex h-1.5 w-10 items-center justify-center rounded-full bg-secondary" />
-                <div className="flex items-center justify-between px-6 py-4">
-                  <div className="flex items-center">
-                    <Avatar.Root className="inline-flex h-14 w-14 select-none items-center justify-center overflow-hidden rounded-full border border-secondary bg-secondary align-middle">
-                      <Avatar.Image
-                        className="h-full w-full rounded-[inherit] object-cover"
-                        src={session?.user?.image ?? ""}
-                        alt="Avatar image"
-                      />
-                      <Avatar.Fallback
-                        className="flex h-full w-full items-center justify-center border border-secondary bg-secondary text-sm font-medium text-primary"
-                        delayMs={600}
-                      >
-                        {session?.user?.name?.slice(0, 2) ?? ""}
-                      </Avatar.Fallback>
-                    </Avatar.Root>
-                    <div className="ml-4">
-                      <p className="font-medium">{session?.user?.name}</p>
-                      <p className="text-sm text-secondary">
-                        {session?.user?.email}
-                      </p>
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => signOut()}
-                    className="rounded-md border border-secondary bg-transparent px-4 py-1.5 text-base"
-                  >
-                    Sign out
-                  </button>
-                </div>
-              </div>
-            </Drawer.Content>
-          </Drawer.Portal>
-        </Drawer.Root> */}
       </>
     );
   }
