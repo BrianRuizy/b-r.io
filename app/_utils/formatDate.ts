2d1e53379422d3039b4e3d53a1aa5007eca49c5a
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-25 04:32:57 -0500

    Update formatDate.ts

--- app/_utils/formatDate.ts
+++ app/_utils/formatDate.ts
@@ -12,7 +12,7 @@ export function formatRelativeTime(date: Date) {
   const diffInSeconds = (now.getTime() - date.getTime()) / 1000;
 
   if (diffInSeconds < 60) return "Just now";
-  if (diffInSeconds < 3600) return `${Math.round(diffInSeconds / 60)}m ago`;
+  if (diffInSeconds < 3600) return `${Math.round(diffInSeconds / 60)}m`;
   if (diffInSeconds < 86400) return `${Math.round(diffInSeconds / 3600)}h`;
   if (diffInSeconds < 604800) return `${Math.round(diffInSeconds / 86400)}d`;
 
