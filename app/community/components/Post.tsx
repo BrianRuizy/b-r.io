c4c96d1799ad9a51ba715b9cc21b491d78f3758b
Brian Ruiz <brianruiz123@yahoo.com>
2024-04-26 22:02:22 -0500

    Fix wrapping and add text-pretty class to post content in Post component

--- app/community/components/Post.tsx
+++ app/community/components/Post.tsx
@@ -97,7 +97,7 @@ export default function Post({ post }: PostComponentProps) {
               topic={{ id: post.topic_id, name: post.topic_name }}
             />
           </span>
-          <span className="ml-auto text-secondary">
+          <span className="ml-auto md:ml-0 text-secondary">
             {/* prettier-ignore */}
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
               <path d="M3 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM8.5 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM15.5 8.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
@@ -105,7 +105,7 @@ export default function Post({ post }: PostComponentProps) {
           </span>
         </div>
         <div
-          className="whitespace-pre-wrap break-words leading-tight"
+          className="whitespace-pre-wrap break-words leading-tight text-pretty md:text-balance"
           style={{ wordBreak: "break-word" }}
           dangerouslySetInnerHTML={{ __html: contentWithLinks }}
         />
