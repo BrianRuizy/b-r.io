299141156d0323d4a1dab2b54790dc942363b297
Brian Ruiz <brianruiz123@yahoo.com>
2024-05-10 03:27:38 -0500

    chore: Update Avatar component to increase size for large avatars

--- app/components/ui/Avatar.tsx
+++ app/components/ui/Avatar.tsx
@@ -20,7 +20,7 @@ export default function Avatar({
         "relative inline-flex select-none items-center justify-center overflow-hidden rounded-full align-middle font-medium uppercase text-primary",
         size === "sm" && "h-10 w-10 bg-tertiary text-sm",
         size === "md" && "h-14 w-14 bg-tertiary text-base",
-        size === "lg" && "h-20 w-20 bg-secondary text-2xl",
+        size === "lg" && "h-24 w-24 bg-secondary text-2xl",
       )}
     >
       {!src || src === "" ? (
