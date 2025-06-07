a029e940faa43a121d32706dd8318c4fd6d9fc3c
Brian Ruiz <brianruiz123@yahoo.com>
2025-06-07 01:57:23 -0400

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { GeistMono } from "geist/font/mono";
 
 import { ThemeProvider } from "@/app/components/ThemeProvider";
 import Navigation from "@/app/components/Navigation";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://b-r.io"),
@@ -26,18 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-        <body className="width-full bg-contrast text-primary antialiased">
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Navigation />
-            <div className="mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20">
-              {children}
-            </div>
-          </ThemeProvider>
-          <Analytics />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="width-full bg-contrast text-primary antialiased">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navigation />
+          <div className="mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20">
+            {children}
+          </div>
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
   );
 }
