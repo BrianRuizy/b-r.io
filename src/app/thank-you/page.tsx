4cc8daff26f13a91c684f6fca2964b9cbd3c7c44
Brian Ruiz <brianruiz0123@gmail.com>
2026-07-29 13:30:33 -0400

    Update page.tsx

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function ThankYou() {
   return (
     <SimpleLayout
-      title="Thanks for subscribing."
+      title="Thank you!"
       intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
     />
   )
